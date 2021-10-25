d87d43f47e461a3eba8b14dbdfa8fd66d910d040
Rajat Jain <rajatjain@Rajats-MacBook-Pro.local>
2021-10-25 17:20:09 +0530

    fixed errors

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
-const router = jsonServer.router;
+const router = jsonServer.router("db.json");
 const PORT = process.env.PORT || 5000;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(PORT, () => console.log(`Server is running at port=${PORT}🚀`))
+server.listen(PORT)
